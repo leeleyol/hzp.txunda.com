4f9699ff6d4186a9347107369d7e26daa09e3f2b
DaGan <775935087@qq.com>
2018-06-16 21:27:51 +0800

    退出登录清除登录状态

--- Public/Home/js/util.js
+++ Public/Home/js/util.js
@@ -3,8 +3,10 @@
 var u = navigator.userAgent;
 var isAndroid = u.indexOf('Android') > -1 || u.indexOf('Adr') > -1; //android终端
 var isiOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); //ios终端
+
+
+
 var m_id = localStorage.getItem('m_id');
-m_id = m_id?m_id:2;
 
 ;(function($){
     $.fn.extend({
