73ca50f3a54649224d9c045a82f793e82241f584
lijianhan <lijianhan@gouuse.cn>
2018-06-25 18:41:55 +0800

    修改供求

---
+++ Application/Manager/Controller/SupplyController.class.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Manager\Controller;
+
+/**
+ * 供求管理
+ * Class OrderController
+ * @package Manager\Controller
+ */
+class SupplyController extends BaseController {
+
+}
---
+++ Application/Manager/Model/SupplyModel.class.php
@@ -0,0 +1,61 @@
+<?php
+namespace Manager\Model;
+
+/**
+ * 订单
+ * Class OrderModel
+ * @package Manager\Model
+ */
+class SupplyModel extends BaseModel {
+
+    /**
+     * @var array
+     * 自动验证规则
+     */
+    protected $_validate = array (
+
+    );
+
+    /**
+     * @var array
+     * 自动完成规则
+     */
+    protected $_auto = array(
+        array('create_time', 'time', self::MODEL_INSERT, 'function'),
+        array('update_time', 'time', self::MODEL_UPDATE, 'function'),
+    );
+
+    /**
+     * @param array $param  综合条件参数
+     * @return array
+     */
+    function getList($param = array()) {
+        if(!empty($param['page_size'])) {
+            $total      = $this->where($param['where'])->count();
+            $Page       = $this->getPage($total, $param['page_size'], $param['parameter']);
+            $page_show  = $Page->show();
+        }
+
+        $model  = $this->where($param['where'])->order($param['order']);
+
+        //是否分页
+        !empty($param['page_size'])  ? $model = $model->limit($Page->firstRow,$Page->listRows) : '';
+
+        $list = $model->select();
+
+        return array('list'=>$list,'page'=>!empty($page_show) ? $page_show : '');
+    }
+
+    /**
+     * @param array $param
+     * @return mixed
+     */
+    function findRow($param = array()) {
+        $row = $this->where($param['where'])->find();
+        $row['goods_list'] = unserialize($row['goods_list']);
+        foreach($row['goods_list'] as $k => $v){
+            $row['goods_list'][$k]['goods_sn'] = M('Goods')->where(array('id'=>$v['goods_id']))->getField('goods_sn');
+        }
+        return $row;
+    }
+}
