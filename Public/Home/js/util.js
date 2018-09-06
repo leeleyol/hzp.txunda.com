ad64e4de1562ac9da84355a66efb9688cb617188
DaGan <775935087@qq.com>
2018-09-06 21:53:32 +0800

    xx

--- Public/Home/js/util.js
+++ Public/Home/js/util.js
@@ -4,7 +4,7 @@ var u = navigator.userAgent;
 var isAndroid = u.indexOf('Android') > -1 || u.indexOf('Adr') > -1; //android终端
 var isiOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); //ios终端
 var m_id = localStorage.getItem('m_id') || 1;
-
+console.log(1)
 ;(function($){
     $.fn.extend({
         "fileInit":function (options) {
