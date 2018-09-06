5e9b5e7a87950cb2585f9358224e28369319bca1
DaGan <775935087@qq.com>
2018-09-06 20:58:00 +0800

    登录判断

--- Public/Home/js/util.js
+++ Public/Home/js/util.js
@@ -3,7 +3,7 @@
 var u = navigator.userAgent;
 var isAndroid = u.indexOf('Android') > -1 || u.indexOf('Adr') > -1; //android终端
 var isiOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); //ios终端
-var m_id = localStorage.getItem('m_id');
+var m_id = localStorage.getItem('m_id') || 1;
 
 
 ;(function($){
